9f6d3edc7316945c420b6b99a76d8c01e0a5afc0
Diego Freire <wdiegosfreire@yahoo.com.br>
2017-11-27 21:48:38 -0300

    temp

--- src/br/com/dfdevforge/sisfin/estabelecimento/persistence/PrsEstabelecimentoDelete.java
+++ src/br/com/dfdevforge/sisfin/estabelecimento/persistence/PrsEstabelecimentoDelete.java
@@ -2,14 +2,15 @@ package br.com.dfdevforge.sisfin.estabelecimento.persistence;
 
 import java.sql.SQLException;
 
-import br.com.cagece.core.persistence.ConnectionManager;
-import br.com.cagece.core.persistence.DeleteStrategy;
+import br.com.cagece.core.persistence.api.ConnectionManager;
+import br.com.cagece.core.persistence.api.DeleteStrategy;
+import br.com.cagece.core.persistence.exception.JpaMappingNotFoundException;
 import br.com.dfdevforge.sisfin.estabelecimento.bean.BtpEstabelecimento;
 import br.com.dfdevforge.sisfin.estabelecimento.bean.BtpEstabelecimentoConfig;
 
 public class PrsEstabelecimentoDelete extends DeleteStrategy<BtpEstabelecimento>
 {
-	public PrsEstabelecimentoDelete(BtpEstabelecimento btpEstabelecimento, ConnectionManager connenction) throws SQLException
+	public PrsEstabelecimentoDelete(BtpEstabelecimento btpEstabelecimento, ConnectionManager connenction) throws SQLException, JpaMappingNotFoundException
 	{
 		this.beanConfig = new BtpEstabelecimentoConfig(btpEstabelecimento);
 		this.connection = connenction;
--- src/br/com/dfdevforge/sisfin/estabelecimento/persistence/PrsEstabelecimentoUpdate.java
+++ src/br/com/dfdevforge/sisfin/estabelecimento/persistence/PrsEstabelecimentoUpdate.java
@@ -2,14 +2,15 @@ package br.com.dfdevforge.sisfin.estabelecimento.persistence;
 
 import java.sql.SQLException;
 
-import br.com.cagece.core.persistence.ConnectionManager;
-import br.com.cagece.core.persistence.UpdateStrategy;
+import br.com.cagece.core.persistence.api.ConnectionManager;
+import br.com.cagece.core.persistence.api.UpdateStrategy;
+import br.com.cagece.core.persistence.exception.JpaMappingNotFoundException;
 import br.com.dfdevforge.sisfin.estabelecimento.bean.BtpEstabelecimento;
 import br.com.dfdevforge.sisfin.estabelecimento.bean.BtpEstabelecimentoConfig;
 
 public class PrsEstabelecimentoUpdate extends UpdateStrategy<BtpEstabelecimento>
 {
-	public PrsEstabelecimentoUpdate(BtpEstabelecimento btpEstabelecimento, ConnectionManager connenction) throws SQLException
+	public PrsEstabelecimentoUpdate(BtpEstabelecimento btpEstabelecimento, ConnectionManager connenction) throws SQLException, JpaMappingNotFoundException
 	{
 		this.beanConfig = new BtpEstabelecimentoConfig(btpEstabelecimento);
 		this.connection = connenction;
